a6abe15f61f32ce575490c28401aabc0f125ddfd
hua <724094526@qq.com>
2022-11-21 17:30:10 +0800

    项目初始化

---
+++ src/main/java/com/bs/bsgl/BsglApplication.java
@@ -0,0 +1,13 @@
+package com.bs.bsgl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsglApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BsglApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bs/bsgl/BsglApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bs.bsgl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsglApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
